e000070369d40a3e5e3d4f4fd8a6ccbfe5941546
hadipournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-02-11 15:38:05 +0000

    I've done difference

--- src/deep-flat/flat.js
+++ src/deep-flat/flat.js
@@ -1,3 +1,8 @@
 let deepFlat = ([1, [2, [3, [4]], 5]]);
 let newFlat = deepFlat.flat(Infinity);
 console.log(newFlat);
+
+
+
+let secondFlat = (['a', ['b', [['c'], ['d']], 'e']]).flat(Infinity);
+console.log(secondFlat);
